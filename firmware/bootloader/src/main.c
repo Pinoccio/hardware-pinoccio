4f85184fb7a0180426f0e04d7d865f386a97a08b
Matthijs Kooijman <matthijs@stdin.nl>
2014-12-31 17:30:03 +0100

    Clean up last code into a minimal example
     - Write nops to make sure that a jump into the main application wraps
       around to the start of the bootloader again.
     - Add led and pin toggles to monitor progress.
     - Add led and pin toggle when bootloader starts without any MCUSR bits
       set.

--- firmware/bootloader/src/main.c
+++ firmware/bootloader/src/main.c
@@ -38,42 +38,65 @@ void __jumpMain(void)
   asm volatile ( "clr __zero_reg__" );                  // GCC depends on register r1 set to 0
   asm volatile ( "out %0, __zero_reg__" :: "I" (_SFR_IO_ADDR(SREG)) );  // set SREG to 0
   asm volatile ( "jmp main");                        // jump to main()
+  asm volatile ( "jmp 0x3e000");                        // jump to main()
 }
 
 //*****************************************************************************
 int main(void)
 {
- 
- MCUSR = 0;
+  // Setup some pins as outputs. PORTB has active low leds, PORTE is for
+  // logic analyzer outputs
+  DDRB |= (1 << PB4) | (1 << PB5) | (1 << PB6);
+  DDRE |= (1 << PE2) | (1 << PE3) | (1 << PE4) | (1 << PE5) | (1 << PE6);
 
-	for(;;) {
-		_delay_ms(20);
+  if (!MCUSR) {
+    // If we get here, we "reset" without a proper hardware reset. This
+    // should never happen.
+    PORTB &= ~(1 << PB6);
+    PORTE |= (1 << PE3);
+    while(1);
+  } else {
+    // Normal startup: Turn off leds (active low), provide trigger to
+    // analyzer
+    PORTB |= (1 << PB4) | (1 << PB5) | (1 << PB6);
+    PORTE |= (1 << PE2);
+  }
+  MCUSR = 0;
 
-			  {
-				unsigned int  size  =  SPM_PAGESIZE;
-				unsigned int  data;
-				uint32_t tempAddress;
-			
-					boot_page_erase(address);  // Perform page erase
-					boot_spm_busy_wait();    // Wait until the memory is erased.	
-					/* Write FLASH */
-					tempAddress = address;
-					do {
-						data    =  0xcfff; // rjmp .-2
-						//data    =  0x0000; // nop
-						boot_page_fill(tempAddress,data);
+  // Write fixed data to the second half of flash
+  uint32_t    address      = (FLASHEND + 1) / 2;
+  for(;;) {
+    // Erase
+    PORTB &= ~(1 << PB4);
+    PORTE &= ~(1 << PE4);
+    boot_page_erase(address);  // Perform page erase
+    boot_spm_busy_wait();    // Wait until the memory is erased.	
+    PORTE |= (1 << PE4);
+    PORTB |= (1 << PB4);
 
-						tempAddress  =  tempAddress + 2;  // Select next word in memory
-						size  -=  2;        // Reduce number of bytes to write by two
-					} while (size);          // Loop until all bytes written
+    // Write
+    uint32_t tempAddress = address;
+    unsigned int  size  =  SPM_PAGESIZE;
+    do {
+      //uint8_t data    =  0xcfff; // rjmp .-2
+      uint8_t data    =  0x0000; // nop
+      boot_page_fill(tempAddress,data);
 
-					boot_page_write(address);
-					boot_spm_busy_wait();
-				address += SPM_PAGESIZE;
-				if (address + SPM_PAGESIZE > APP_END + 1)
-				  address = (FLASHEND + 1) / 2;
-			  }
+      tempAddress  =  tempAddress + 2;  // Select next word in memory
+      size  -=  2;        // Reduce number of bytes to write by two
+    } while (size);          // Loop until all bytes written
 
+    PORTE &= ~(1 << PE5);
+    PORTB &= ~(1 << PB5);
+    boot_page_write(address);
+    boot_spm_busy_wait();
+    PORTE |= (1 << PE5);
+    PORTB |= (1 << PB5);
 
-	}
+    // Wrap around from APP_END to halfway
+    // FLASHEND
+    address += SPM_PAGESIZE;
+    if (address + SPM_PAGESIZE > APP_END + 1)
+      address = (FLASHEND + 1) / 2;
+  }
 }
