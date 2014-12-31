e201a7af3f16578c76e23e28ac86e361229c986d
Matthijs Kooijman <matthijs@stdin.nl>
2014-12-31 19:26:49 +0100

    Fix comments to keep github disassembly syntax highlighting happy

--- firmware/bootloader/src/main.c
+++ firmware/bootloader/src/main.c
@@ -19,15 +19,15 @@
 void __jumpMain  (void) __attribute__ ((naked)) __attribute__ ((section (".init9")));
 #include <avr/sfr_defs.h>
 
-//*****************************************************************************
+//*****************************************************************************/
 void __jumpMain(void)
 {
-//*  July 17, 2010  <MLS> Added stack pointer initialzation
-//*  the first line did not do the job on the ATmega128
+//  July 17, 2010  <MLS> Added stack pointer initialzation
+//  the first line did not do the job on the ATmega128
 
   asm volatile ( ".set __stack, %0" :: "i" (RAMEND) );
 
-//*  set stack pointer to top of RAM
+//  set stack pointer to top of RAM
 
   asm volatile ( "ldi  16, %0" :: "i" (RAMEND >> 8) );
   asm volatile ( "out %0,16" :: "i" (AVR_STACK_POINTER_HI_ADDR) );
@@ -41,7 +41,7 @@ void __jumpMain(void)
   asm volatile ( "jmp 0x3e000");                        // jump to main()
 }
 
-//*****************************************************************************
+//*****************************************************************************/
 int main(void)
 {
   // Setup some pins as outputs. PORTB has active low leds, PORTE is for
