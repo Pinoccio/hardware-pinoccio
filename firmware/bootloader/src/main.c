b8cb49c8a8cc856e1d0f1fce3f7b96924faa6d65
Matthijs Kooijman <matthijs@stdin.nl>
2014-12-31 16:30:50 +0100

    Remove more stuff

--- firmware/bootloader/src/main.c
+++ firmware/bootloader/src/main.c
@@ -12,17 +12,12 @@
 
 #define APP_END  (FLASHEND -(2*BOOTSIZE) + 1)
 
-typedef uint32_t address_t;
-
 /*
  * since this bootloader is not linked against the avr-gcc crt1 functions,
  * to reduce the code size, we need to provide our own initialization
  */
 void __jumpMain  (void) __attribute__ ((naked)) __attribute__ ((section (".init9")));
 #include <avr/sfr_defs.h>
-
-//#define  SPH_REG  0x3E
-//#define  SPL_REG  0x3D
 
 //*****************************************************************************
 void __jumpMain(void)
@@ -48,33 +43,8 @@ void __jumpMain(void)
 //*****************************************************************************
 int main(void)
 {
-  address_t    address      = (FLASHEND + 1) / 2;
-
-  //*  some chips dont set the stack properly
-  asm volatile ( ".set __stack, %0" :: "i" (RAMEND) );
-  asm volatile ( "ldi  16, %0" :: "i" (RAMEND >> 8) );
-  asm volatile ( "out %0,16" :: "i" (AVR_STACK_POINTER_HI_ADDR) );
-  asm volatile ( "ldi  16, %0" :: "i" (RAMEND & 0x0ff) );
-  asm volatile ( "out %0,16" :: "i" (AVR_STACK_POINTER_LO_ADDR) );
-
-
- //************************************************************************
- /* Feb 11, 2014  Use WDT to enter OTA bootloader section
-  * Do not clear WDT status flag, leave that for the main app
-  */
- GPIOR0 = MCUSR;	// store status register in GPIOR0 to provide feedback on reset reason to main app
  
- // make sure watchdog is off!
- __asm__ __volatile__ ("cli");
- __asm__ __volatile__ ("wdr");
  MCUSR = 0;
- WDTCSR  |=  _BV(WDCE) | _BV(WDE);
- WDTCSR  =  0;
- __asm__ __volatile__ ("sei");
-   
-
-  asm volatile ("nop");      // wait until port has changed
-
 
 	for(;;) {
 		_delay_ms(20);
@@ -82,15 +52,15 @@ int main(void)
 			  {
 				unsigned int  size  =  SPM_PAGESIZE;
 				unsigned int  data;
-				address_t tempAddress;
+				uint32_t tempAddress;
 			
 					boot_page_erase(address);  // Perform page erase
 					boot_spm_busy_wait();    // Wait until the memory is erased.	
-								  
 					/* Write FLASH */
 					tempAddress = address;
 					do {
 						data    =  0xcfff; // rjmp .-2
+						//data    =  0x0000; // nop
 						boot_page_fill(tempAddress,data);
 
 						tempAddress  =  tempAddress + 2;  // Select next word in memory
