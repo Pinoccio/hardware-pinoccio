b3d6a0980749c82daabbd01828841e49e6eeebc4
Matthijs Kooijman <matthijs@stdin.nl>
2015-03-06 12:58:07 +0100

    16u2: Remove DFU-related stuff
    Since we're not using the DFU bootloader, better remove it. combine.sh
    is for combining the firmware with the bootloader and the readme also
    assumes DFU is used, which would only be confusing.

--- firmware/atmega16u2/lufa-100807/Projects/arduino-usbserial/combine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-(sed '$d' Arduino-usbserial.hex; cat Arduino-usbdfu.hex) > pinoccio-16U2-combined.hex
-
-echo "Install combined hex with the following avrdude command:"
-echo "avrdude -p m16u2 -P usb -c avrisp2 -U flash:w:pinoccio-16U2-combined.hex:m -U lfuse:w:0xEF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m"
